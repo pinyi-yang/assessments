ee21ddde14f7763a50c73af3fe014d5db5ef07fe
sixhops <sixhops@gmail.com>
2019-06-16 18:34:51 -0700

    Adding solution code

---
+++ solution-code/js/main.js
@@ -0,0 +1,36 @@
+
+/*----- app state (variables) -----*/
+var count;
+
+/*----- DOM element references -----*/
+var displayEl = document.querySelector('h1');
+var inputEl = document.querySelector('input');
+
+/*----- event listeners -----*/
+document.getElementById('plus-btn').addEventListener('click', increment);
+document.getElementById('minus-btn').addEventListener('click', decrement);
+
+/*----- functions -----*/
+function increment() {
+  count += parseInt(inputEl.value);
+  render();
+}
+
+function decrement() {
+  count -= parseInt(inputEl.value);
+  render();
+}
+
+function initialize() {
+  count = 0;
+  inputEl.value = 1;
+  render();
+}
+
+function render() {
+  displayEl.textContent = count;
+  displayEl.className = count < 0 ? 'red' : '';
+}
+
+/*----- execute -----*/
+initialize();
